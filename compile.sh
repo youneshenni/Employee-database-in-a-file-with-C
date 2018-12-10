516033e6b0b540e2b687819d5a75fb3e2104a908
Younes Henni <36455366+younabobo@users.noreply.github.com>
2018-12-10 18:02:27 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@ clang -c headers/list.c -o object/list.o
 clang -c headers/struct.c -o object/struct.o
 clang -c main.c -o object/main.o
 
-clang -o exo2.out object/main.o object/file.o object/list.o object/struct.o
+clang -o main.out object/main.o object/file.o object/list.o object/struct.o
